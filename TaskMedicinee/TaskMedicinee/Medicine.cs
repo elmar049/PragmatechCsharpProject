885b08c91fc21e1531533ca64125cece180014b1
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-01-19 17:18:37 +0100

    TaskMedicine

---
+++ TaskMedicinee/TaskMedicinee/Medicine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMedicinee
+{
+    internal class Medicine
+    {
+        public string Name;
+        public int Price;
+        public int Count;
+
+
+
+        public Medicine(string ad, int qiymet)
+        {
+            Name = ad;
+            Price = qiymet;
+            
+        }
+
+        public void Sell(string dermanAdi, int dermanSayi, Medicine[] medicines)
+        {
+            if (dermanAdi==Name && dermanSayi <= Count)
+            {
+                int qaliq = Count - dermanSayi;
+                Console.WriteLine($"Bu {Name}-indan {qaliq} qeder qalib ");
+            }
+            else
+            {
+                Console.WriteLine("Depoda bu qeder derman yoxudr");
+            }
+                  
+        }
+    }
+}
