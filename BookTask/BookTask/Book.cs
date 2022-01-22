4ea49ebf55af759b10122c81ee6be11e073515bf
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-01-22 11:14:05 +0100

    Update

---
+++ BookTask/BookTask/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTask
+{
+    public class Book
+    {
+        public string Name;
+        public int PageNumber;
+
+        public Book(string name, int pagenumber)
+        {
+            Name = name;
+            PageNumber = pagenumber;
+        }
+
+
+
+    }
+
+    
+}
