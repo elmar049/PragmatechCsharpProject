0cb5258dc8d82cbfcf6f378ac275e521590ec018
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-01-27 22:09:05 +0100

    TAsk Folder

---
+++ task/task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student telebe1 = new Student("elmar", "memmedov", 28);
+            Student telebe2 = new Student("mina", "ferzeli", 18);
+
+
+        }
+
+    }
+}
