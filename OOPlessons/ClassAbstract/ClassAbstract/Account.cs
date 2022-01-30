035e0d2a258652c72c3a8f9132bf32024b038834
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-01-30 11:09:55 +0100

    OOP Tasks

---
+++ OOPlessons/ClassAbstract/ClassAbstract/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAbstract
+{
+    abstract class Account
+    {
+
+    }
+
+
+
+    private showall()
+}
