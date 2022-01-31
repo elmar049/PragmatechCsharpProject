cc0fe9ec59571df5b9eec860549edcef63f080ec
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-01-31 21:04:42 +0100

    Private Fields of Class and its overloading

---
+++ Draft/StaticClass/StaticClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student("elmar", 28);
+            //std.Print();
+
+
+
+
+            Student std2 = new Student("elamr", "mammadov", 28, new DateTime(1993, 08, 23));
+            Student std3 = std2; // eto dva ssilochnie tipi dannix, kotoroe otnosyatda k odnomu istochniku...esli izmenaetsa odin to i izmenaetsa vtoroy
+            std2.ChangeSurname("huseynov");
+
+
+            std2.Print();
+            std3.Print(); // zdes u oboix budet familiya HUSEYNOV
+
+
+
+
+
+            //no ESLI
+            Student std4 = new Student("elamr", "mammadov", 28, new DateTime(1993, 08, 23));
+            Student std5 = new Student(std4);
+            std4.ChangeSurname("aliyeba");
+
+            std4.Print();
+            std5.Print();
+
+
+
+
+
+
+            
+        }
+    }
+}
---
+++ Draft/StaticClass/StaticClass/student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    class Student
+    {
+        private string _Name;
+        private string _surName;
+        private int _Age;
+        private DateTime _Birth;
+
+
+
+        public Student(string name, int age)
+        {
+            _Name= name;
+            _Age= age;  
+        }
+
+
+
+        public Student(string name, string sruname, int age, DateTime birth)
+        {
+            _Name = name;
+            _surName= sruname;
+            _Age = age;
+            _Birth= birth;
+            
+        }
+
+
+
+        public Student(Student telebe)
+        {
+            _Name = telebe._Name;
+            _surName = telebe._surName;
+            _Age = telebe._Age;
+            _Birth = telebe._Birth;
+
+        }
+
+        public string ChangeSurname(string x)
+        {
+            this._surName = x;
+            return _surName;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {_Name} \nSurname: {_surName} \n{_Age} \n{_Birth} ");
+        }
+
+
+
+
+
+    }
+
+}
+        
+
+
+   
+
+
+
