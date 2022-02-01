23828d0554d40eebd89e8ada05e23e65ab3177b6
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-01 20:35:33 +0100

    Task about inheritence

---
+++ HomeWorkTask/OOP. Abstact Class Task/OOP. Abstact Class Task/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Abstact_Class_Task
+{
+    abstract class Account
+    {
+        public string FullName;
+        public string Email;
+        public string Password;
+
+
+        public abstract bool PasswordChecker(string pass);
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine(FullName + " " + Email);
+        }
+    }
+
+    class user : Account
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Full Name is gonna be: {this.FullName} \nMail is: {this.Email} ");
+        }
+
+        public override bool PasswordChecker(string pass)
+        {
+
+            bool x = false;
+            bool y = false;
+            bool a = false;
+
+
+            if (pass.Length > 8)
+            {
+                x = true;
+            }
+
+            if (x)
+            {
+                char[] xxx = pass.ToCharArray();
+                foreach (char c in xxx)
+                {
+                    
+                    if (char.IsUpper(c)) 
+                    {
+                        y = true;
+                       
+                    }
+                    if (char.IsLower(c) && y)
+                    {
+                        a = true;
+                    }
+                }
+            }
+
+            return a;
+        }
+
+
+    }
+}
+
---
+++ HomeWorkTask/OOP. Abstact Class Task/OOP. Abstact Class Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Abstact_Class_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            user user = new user();
+            user.FullName = "Elmar Mammadov";
+            user.Email = "mammadovelmar93@gmail.com";
+            user.Password = "JSJSJSJJaJJJJ";
+
+
+
+            user.ShowInfo();
+            Console.WriteLine("Your pass is " + user.PasswordChecker(user.Password));
+
+        }
+    }
+}
