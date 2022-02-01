da10c7f19300b90e0d6719e720e070f8017f69cd
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-01 15:11:39 +0100

    Task

---
+++ Draft/GetSetInheritence/GetSetInheritence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetInheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Men man = new Men("elmar", "mammadov", 15, 5);
+            man.GetFullInfo();
+            
+        }
+    }
+}
---
+++ Draft/GetSetInheritence/GetSetInheritence/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetInheritence
+{
+   abstract class Student  // zdes est Abstarct, poetomu mi ne mojem sozdat obyekt
+    {       
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public  virtual void GetFullInfo()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+    }
+
+    class Girls: Student
+    {
+        public int HairLength;
+
+        public Girls(string ad, string soyad, int sacuzunlugu): base(ad, soyad)
+        {
+            HairLength = sacuzunlugu;
+        }
+    }
+
+    class Men: Girls
+    {
+        public int BeardLength;
+
+        public Men(string add, string soyadd, int sacuzuluguu, int sakkal): base(add, soyadd, sacuzuluguu)
+        {
+            BeardLength = sakkal;
+        }
+
+        public override void GetFullInfo()
+        {
+            Console.WriteLine(this.Name + " " + this.Surname + " ve sakkal uzunlugu " + BeardLength);
+        }
+    }
+}
