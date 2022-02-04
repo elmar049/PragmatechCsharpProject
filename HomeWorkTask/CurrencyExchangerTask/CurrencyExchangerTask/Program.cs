4fda9d2c69d4ac3c47c902ded64ef7f77214249d
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-04 23:43:30 +0100

    CurrencyExchangerTask

--- HomeWorkTask/CurrencyExchangerTask/CurrencyExchangerTask/Program.cs
+++ HomeWorkTask/CurrencyExchangerTask/CurrencyExchangerTask/Program.cs
@@ -10,29 +10,75 @@ namespace CurrencyExchangerTask
     {
         static void Main(string[] args)
         {
+
+            while (true)
+            {
+                Console.WriteLine("Please Choose the currency you want to exchange: \n1. USD \n2. EU \n3. Tl ");
+
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Now please write the amount of money");
+                double input = Convert.ToDouble(Console.ReadLine());
+
+                if (x == 1)
+                {
+                    CurrencyExchanger.Exchange(CurrencyType.USD, input);
+
+                }
+                if (x == 2)
+                {
+                    CurrencyExchanger.Exchange(CurrencyType.Eu, input);
+
+                }
+                if (x == 3)
+                {
+                    CurrencyExchanger.Exchange(CurrencyType.TL, input);
+
+                }
+            }
            
+
         }
     }
 
 
 
-
-
-
-
-    public class CurrencyExchanger
+    static class CurrencyExchanger
     {
-        public CurrencyType type;
-        public Dictionary<CurrencyType, double > currency;
+
+      
+        public static Dictionary<CurrencyType, double> currency = new Dictionary<CurrencyType, double>();
+        
+
+        public static void Exchange(CurrencyType tayp, double pul)
+        {
+            
+            currency.Add(tayp, pul);
+            double x = 0;
+            if (tayp == CurrencyType.Eu)
+            {
+                x = pul * 2;
+            }
+            else if (tayp == CurrencyType.USD)
+            {
+                x = pul * 1.7;
+            }
+            else if (tayp == CurrencyType.TL)
+            {
+                x = pul * 0.19;
+            }
+
+            Console.WriteLine($"Sizin daxil elediyniz {pul} {tayp}-iniz, indiki kursla {x} azn eleyir");
+        }
+
 
     }
-       
+
 
     public enum CurrencyType
     {
-        USD,
-        Eu,
-        TL,
+        USD=1,
+        Eu=2,
+        TL=3,
     }
 }
 
