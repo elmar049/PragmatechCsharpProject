4142160cc11d011d832e8f7d39f6acfb287739d2
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-04 21:29:46 +0100

    OOP takss

---
+++ HomeWorkTask/CurrencyExchangerTask/CurrencyExchangerTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchangerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+
+
+
+
+
+
+    public class CurrencyExchanger
+    {
+        public CurrencyType type;
+        public Dictionary<CurrencyType, double > currency;
+
+    }
+       
+
+    public enum CurrencyType
+    {
+        USD,
+        Eu,
+        TL,
+    }
+}
+
