54822b21201ac5bfcf576dcb4ac5aebf682ae9f6
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-10 19:39:12 +0100

    game

--- Draft/Dictionary/Dictionary/Program.cs
+++ Draft/Dictionary/Dictionary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Dictionary
 {
@@ -24,55 +25,91 @@ namespace Dictionary
 
 
 
+            Console.WriteLine("Welcome to the game of Elmar. You will get a word, your goal is determine what is gonna be the second one. \nFirst attempt will give 6 points \nSecond attempt will give 4 points  \nThird attempt will give 2 points.  \nYou will have a 2 min. \nPress 1 and you will start and you time will be countdown: ");
+            int one = Convert.ToInt32(Console.ReadLine());
+            if (one == 1)
+            {
+                int num = 0;
+                TimerCallback tm = new TimerCallback(Count();
+                Timer timer = new Timer(tm, num, 20000, 0);
+
+                RandomWord(strlist);
+            }
 
 
-            RandomWord(strlist);
 
-
+            //Console.ReadLine();
 
 
         }
 
 
+        public static void Count(object obj, )
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Time is UP, lets calculate your results, be away from a keyboard. Dont Touch it");
+            RandomWord
 
+
+        }
+
+
+        
 
         public static void RandomWord(string[] sozler)
         {
-            int score = 0;
-            var idx = new Random().Next(sozler.Length);
-            Console.WriteLine(sozler[idx]);
-            Console.WriteLine("Please insert the wordd you think will be the next");
-            string input = Console.ReadLine();
+            int countofexp = 0;
+            while (true)
+            {
 
-            if (input == sozler[idx + 1])
-            {
-                Console.WriteLine("you got a 6 coins");
-                score += 6;
-            }
-            else
-            {
-                Console.WriteLine("You were wrong, and you lost to get 6 coins try once more");
-                string input2 = Console.ReadLine();
-                if (input2 == sozler[idx + 1])
+                int score = 0;
+                var idx = new Random().Next(sozler.Length);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sozler[idx]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Please insert the word you think will be the next");
+                string input = Console.ReadLine();
+
+                if (input == sozler[idx + 1])
                 {
-                    Console.WriteLine("You got a 4 coins");
-                    score += 4;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("you got a 6 coins");
+                    score += 6;
                 }
                 else
                 {
-                    Console.WriteLine("You were wrong, and you lost to get 4 coins, try once more");
-                    string input3 = Console.ReadLine();
-                    if (input3 == sozler[idx + 1])
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("You were wrong, and you lost to get 6 coins try once more");
+                    string input2 = Console.ReadLine();
+                    if (input2 == sozler[idx + 1])
                     {
-                        Console.WriteLine("you got a 3 coins");
-                        score += 3;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("You got a 4 coins");
+                        score += 4;
                     }
                     else
                     {
-                        Console.WriteLine("That is enough let play another game");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("You were wrong, and you lost to get 4 coins, try once more");
+                        string input3 = Console.ReadLine();
+                        if (input3 == sozler[idx + 1])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("you got a 2 coins");
+                            score += 2;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is enough let play another round");
+                        }
                     }
                 }
+                countofexp++;
             }
+
+
         }
 
 
---
+++ Draft/timer/timer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace timer
+{
+    internal class Program
+    {
+        private static Timer aTimer;
+
+        static void Main(string[] args)
+        {
+            // Create a timer and set a two second interval.
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = 2000;
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            aTimer.AutoReset = true;
+
+            // Start the timer
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        }
+
+    }
+}
