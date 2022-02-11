0c77acd91b80c74bbf7be5dfe6b92f0b92aec018
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-11 22:21:05 +0100

    updates

---
+++ Draft/timer/timer/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timer
+{
+    class VirtualCard
+    {
+        private long _cardnumber;
+        //public double cash;
+        //public int lifetime;
+
+        public long cardnumber
+        {
+            get { return _cardnumber; }
+            set
+            {
+                Random rd = new Random();
+                string num = "";
+                for(int i=0; i<16; i++)
+                {
+                   
+                    int rand_num = rd.Next(0, 9);
+                    
+                    num = num + rand_num;
+                }
+
+                value = long.Parse(num);
+                _cardnumber = value;
+
+
+            }
+        }
+
+
+
+    }
+}
--- Draft/timer/timer/Program.cs
+++ Draft/timer/timer/Program.cs
@@ -9,38 +9,20 @@ namespace timer
 {
     internal class Program
     {
-        private static Timer aTimer;
+        
 
         static void Main(string[] args)
         {
-            // Create a timer and set a two second interval.
-            aTimer = new System.Timers.Timer();
-            aTimer.Interval = 1000;
+            VirtualCard card1=new VirtualCard();
 
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-
-            // Have the timer fire repeated events (true is the default)
-            aTimer.AutoReset = true;
-
-            // Start the timer
-            aTimer.Enabled = true;
-
-            Console.WriteLine("Press the Enter key to exit the program at any time... ");
-            Console.ReadLine();
-
-
-
-
-
-
-
+            
+            
+            Console.WriteLine(card1.cardnumber);
 
         }
-        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
-        {
-            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
-        }
+        
 
     }
+
+
 }
