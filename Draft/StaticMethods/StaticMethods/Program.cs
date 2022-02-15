af1177b7be4494059758fa230dfb41d995139f76
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-15 17:49:10 +0100

    Static Methods

---
+++ Draft/StaticMethods/StaticMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethods
+{
+    //Static method sirtigdi
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj=new MyClass();
+            MyClass.a = 8;  //odin raz izmeni i vse on vezde 8
+
+            Console.WriteLine(MyClass.a);
+            MyClass.method2();
+
+            obj.Method1();
+
+
+
+
+
+            //MyClass.method2();
+
+        }
+    }
+    public class MyClass
+    {
+        public static int a=5;
+        public int b;
+
+
+        public static void method2()
+        {
+            Console.WriteLine("static method");
+            
+            Console.WriteLine(a);
+           
+
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("Non-Static method");
+            Console.WriteLine(a);
+           
+
+            
+        }
+
+        
+    }
+}
