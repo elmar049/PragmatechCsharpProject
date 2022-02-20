c11a9daf22750b89b049574e780726b78af8b609
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-20 18:56:27 +0100

    draft

---
+++ Draft/draft/draft/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace draft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface idrescode
+    {
+        void Data(string salam );
+
+    }
+
+    public class uuu : idrescode
+    {
+        public void Data(string salam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
