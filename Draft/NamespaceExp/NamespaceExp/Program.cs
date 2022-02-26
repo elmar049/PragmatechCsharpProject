059bd5a778e06f170434992efc958c03ab6c1e45
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-02-26 15:11:43 +0100

    updates

---
+++ Draft/NamespaceExp/NamespaceExp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceExp
+{
+    public class Off
+    {
+        public  int x;   // it should be static or not? why here is an mistake...
+    }
+    internal class Program
+    {
+
+    }
+}
+
+namespace NamespaceExp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("salam2");
+            //NamespaceExp.Off.x = 5;
+            //Console.WriteLine(NamespaceExp.Off.x);  // for static fields
+
+            NamespaceExp.Off offfff = new NamespaceExp.Off();
+            offfff.x = 10;
+            Console.WriteLine(offfff.x);
+
+
+        }
+    }
+}
