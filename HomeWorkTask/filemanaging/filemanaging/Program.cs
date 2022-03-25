2c4f864e940ee485f799477cf982bf4f8e20efb7
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-03-25 14:28:43 +0100

    upds

---
+++ HomeWorkTask/filemanaging/filemanaging/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace filemanaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathh = @"C:\Users\elmar\Desktop\demo";
+
+            var x = Directory.GetFiles(pathh);
+
+            foreach (var f in x)
+            {
+                var o = new FileInfo(f);
+                Console.WriteLine(o.Length);
+            }
+        }
+    }
+}
