fdd7bca4a3d2c1bc239ff55d028480771ab44f93
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-03-31 00:08:03 +0200

    sql

--- Lessons/PhotoCallery/PhotoCallery/Form1.cs
+++ Lessons/PhotoCallery/PhotoCallery/Form1.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace PhotoCallery
 {
     public partial class Form1 : Form
     {
+        string RootPathFolder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
+
         public Form1()
         {
             InitializeComponent();
 
             int left = 30;
+           
 
             List<string> clickes = new List<string>();
             clickes.Add("Tebiyet");
@@ -25,12 +30,12 @@ namespace PhotoCallery
 
             List<string> photos = new List<string>() {
 
-                "demo/02.JPG",
-                "demo/1.jpg",
-                "demo/03.jpg",
+                "/demo/02.JPG",
+                "/demo/1.jpg",
+                "/demo/03.jpg",
 
             };
-            jj
+            
             foreach (string click in clickes)
             {
                 Button label = new Button();
@@ -43,29 +48,49 @@ namespace PhotoCallery
 
             foreach (string photo in photos)
             {
+                
                 PictureBox pictureBox = new PictureBox();
-                pictureBox.Image = Image.FromFile(@"C:\Users\elmar\Desktop\demo\1.jpg");
+                pictureBox.Image = Image.FromFile($"{ RootPathFolder}{photo}");
                 pictureBox.Top = 80;
                 pictureBox.Height = 300;
                 pictureBox.Width = 300;
                 pictureBox.Left = left;
                 left += 150;
                 this.Controls.Add(pictureBox);
+
+          
+               
+
+                
+
+               
             }
+             
 
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //openFileDialog1.ShowDialog();
-            //string path = openFileDialog1.FileName;
-            //pictureBox1.Image = Image.FromFile(path);  
+            
+            var OFD = new OpenFileDialog();
+            OFD.ShowDialog();
+
+            var RootPath = OFD.FileName;
+            var DestinationPath = $"{RootPathFolder}/demo/";
+            var NewNamw = new Random().Next(0, 1000);
+            var GetExt=Path.GetExtension(RootPath);
+            File.Copy(RootPath, $"{DestinationPath}{NewNamw}{GetExt}");
+
+
+            pictureBox.Image = Image.FromFile($"{DestinationPath}{NewNamw}.{GetExt}");
+
+
+            MessageBox.Show(OFD.FileName.ToString());
+
+
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
