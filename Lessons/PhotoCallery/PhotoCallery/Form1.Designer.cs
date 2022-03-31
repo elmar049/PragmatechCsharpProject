65716a298f277e518137153dbf0d1621a0c13282
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-04-01 01:28:05 +0200

    upds

--- Lessons/PhotoCallery/PhotoCallery/Form1.Designer.cs
+++ Lessons/PhotoCallery/PhotoCallery/Form1.Designer.cs
@@ -30,14 +30,25 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.category_button = new System.Windows.Forms.Button();
+            this.category_text = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(814, 22);
+            this.button1.Location = new System.Drawing.Point(925, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(68, 29);
+            this.button1.Size = new System.Drawing.Size(81, 34);
             this.button1.TabIndex = 3;
             this.button1.Text = "Upload";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,21 +58,122 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(885, 12);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 24);
+            this.comboBox.TabIndex = 5;
+            // 
+            // category_button
+            // 
+            this.category_button.Location = new System.Drawing.Point(699, 40);
+            this.category_button.Name = "category_button";
+            this.category_button.Size = new System.Drawing.Size(121, 34);
+            this.category_button.TabIndex = 6;
+            this.category_button.Text = "Category add";
+            this.category_button.UseVisualStyleBackColor = true;
+            this.category_button.Click += new System.EventHandler(this.category_button_Click);
+            // 
+            // category_text
+            // 
+            this.category_text.Location = new System.Drawing.Point(688, 12);
+            this.category_text.Name = "category_text";
+            this.category_text.Size = new System.Drawing.Size(132, 22);
+            this.category_text.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 65535);
+            this.panel1.TabIndex = 8;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // categoriesToolStripMenuItem
+            // 
+            this.categoriesToolStripMenuItem.Name = "categoriesToolStripMenuItem";
+            this.categoriesToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.categoriesToolStripMenuItem.Text = "Categories";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.categoriesToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1062, 28);
+            this.menuStrip2.TabIndex = 10;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 28);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1062, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(910, 490);
+            this.ClientSize = new System.Drawing.Size(1062, 542);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.category_text);
+            this.Controls.Add(this.category_button);
+            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Gallery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button category_button;
+        private System.Windows.Forms.TextBox category_text;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
--- Lessons/PhotoCallery/PhotoCallery/Form1.cs
+++ Lessons/PhotoCallery/PhotoCallery/Form1.cs
@@ -14,83 +14,192 @@ namespace PhotoCallery
 {
     public partial class Form1 : Form
     {
+        int left = 30;
         string RootPathFolder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
+        PhotoCalleryEntities db = new PhotoCalleryEntities();
+        int FindRightIndex;
+
+
+        public void ComboList()
+        {
+
+            var comboboxlist = new List<string>();
+            foreach (var category in db.Categories.ToList())
+            {
+                comboboxlist.Add(category.CategoryName);
+
+            }
+
+            comboBox.DataSource = comboboxlist;
+
+        }
+        public void InsertingPhotoToPanel(string fullyname)
+        {
+            var pcb = new PictureBox();
+            pcb.Image = Image.FromFile(fullyname);
+            pcb.Top = 5;
+            pcb.Width = 160;
+            pcb.Height = 160;
+            pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcb.Left = left;
+            left += pcb.Width + 20;
+            panel1.Controls.Add(pcb);
+
+            foreach(var x in panel1.Controls)
+            {
+                if (panel1.Controls.Count % 4 == 0)
+                {
+                    pcb.Top = 170;
+                    pcb.Left = 30;
+                }
+            }
+        }
+
+        public object AddingPhoto(DateTime daatetime, float FileSize, string FileLocation, string GetExt, int NewNamw)
+        {
+            var PhotoFileExample = new Photos() {
+                fileNamee = $"{NewNamw}{GetExt}",
+                fileAddDate = daatetime,
+                fileSize = FileSize,
+                fileExtention = GetExt,
+                fileLocation = $"demo/{FileLocation}",
+                fileCategoryId = FindRightIndex
+
+            };
+            db.Photos.Add(PhotoFileExample);
+            db.SaveChanges();
+            return PhotoFileExample;
+
+
+
+        }
+
 
         public Form1()
         {
             InitializeComponent();
+            ComboList();
 
-            int left = 30;
-           
 
-            List<string> clickes = new List<string>();
-            clickes.Add("Tebiyet");
-            clickes.Add("Auto");
-            clickes.Add("Human");
 
-            List<string> photos = new List<string>() {
 
-                "/demo/02.JPG",
-                "/demo/1.jpg",
-                "/demo/03.jpg",
 
-            };
-            
-            foreach (string click in clickes)
+
+
+
+
+
+
+            foreach (var click in db.Categories.ToList())
             {
                 Button label = new Button();
-                label.Text = click;
-                label.Top = 30;
+                label.Text = click.CategoryName;
+                label.Top = 80;
                 label.Left = left;
-                left += label.Width;
+                left += label.Width + 20;
                 this.Controls.Add(label);
+                label.Click += Clickoncategory;
+
             }
-
-            foreach (string photo in photos)
-            {
-                
-                PictureBox pictureBox = new PictureBox();
-                pictureBox.Image = Image.FromFile($"{ RootPathFolder}{photo}");
-                pictureBox.Top = 80;
-                pictureBox.Height = 300;
-                pictureBox.Width = 300;
-                pictureBox.Left = left;
-                left += 150;
-                this.Controls.Add(pictureBox);
-
-          
-               
-
-                
-
-               
-            }
-             
-
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            foreach (var click in db.Categories.ToList())
+                if (comboBox.SelectedItem.ToString() == click.CategoryName)
+                {
+                    FindRightIndex = click.id;
+                }
+
+
+
             var OFD = new OpenFileDialog();
             OFD.ShowDialog();
 
             var RootPath = OFD.FileName;
             var DestinationPath = $"{RootPathFolder}/demo/";
             var NewNamw = new Random().Next(0, 1000);
-            var GetExt=Path.GetExtension(RootPath);
+            var GetExt = Path.GetExtension(RootPath);
+            DateTime daatetime = DateTime.Now;
+            var FileSize = new FileInfo(RootPath).Length;
+            var FileLocation = $"{NewNamw}{GetExt}";
+            var FullnewName = DestinationPath + NewNamw + GetExt;
             File.Copy(RootPath, $"{DestinationPath}{NewNamw}{GetExt}");
+         
 
-
-            pictureBox.Image = Image.FromFile($"{DestinationPath}{NewNamw}.{GetExt}");
-
-
-            MessageBox.Show(OFD.FileName.ToString());
-
+            AddingPhoto(daatetime, FileSize, FileLocation, GetExt, NewNamw);           
+            InsertingPhotoToPanel(FullnewName);
 
         }
 
-       
+
+        private void category_button_Click(object sender, EventArgs e)
+        {
+            var insertedCategory = category_text.Text;
+            var newCateg = new Categories() {
+                CategoryName = insertedCategory,
+            };
+
+            db.Categories.Add(newCateg);
+            db.SaveChanges();
+            ComboList();
+
+        }
+        public void Clickoncategory(object sender, EventArgs e)
+        {
+
+            var btn = sender as Button;
+            foreach (var category in db.Categories.ToList())
+            {
+                if (category.CategoryName == btn.Text)
+                {
+                    FindRightIndex = category.id;
+
+                }
+            }
+
+
+            var SelectedPhotos = new List<Photos>();
+            foreach (var photo in db.Photos.ToList())
+            {
+                if (FindRightIndex == photo.fileCategoryId)
+                {
+                    SelectedPhotos.Add(photo);
+
+                }
+            }
+
+            panel1.Controls.Clear();
+            left = 30;
+            foreach (var photo in SelectedPhotos)
+            {
+                string fullynewname = $"{RootPathFolder}/demo/{photo.fileNamee}";
+                InsertingPhotoToPanel(fullynewname);
+
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void categoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+            foreach (var rw in db.Categories.ToList())
+            {
+                
+              ToolStripMenuItem MnuStripItem = new ToolStripMenuItem();
+                MnuStripItem.Text = rw.CategoryName;
+            }
+
+        }
+
+        
     }
 }
