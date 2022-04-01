a9157f4e65f2219b333f7466c7a8fe8cae4ae67b
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-04-01 16:42:36 +0200

    photogallery

--- Lessons/PhotoCallery/PhotoCallery/Form1.Designer.cs
+++ Lessons/PhotoCallery/PhotoCallery/Form1.Designer.cs
@@ -87,7 +87,7 @@
             // 
             this.panel1.Location = new System.Drawing.Point(0, 157);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 65535);
+            this.panel1.Size = new System.Drawing.Size(2000, 65535);
             this.panel1.TabIndex = 8;
             // 
             // fileToolStripMenuItem
--- Lessons/PhotoCallery/PhotoCallery/Form1.cs
+++ Lessons/PhotoCallery/PhotoCallery/Form1.cs
@@ -15,9 +15,11 @@ namespace PhotoCallery
     public partial class Form1 : Form
     {
         int left = 30;
+        int top = 5;
         string RootPathFolder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
         PhotoCalleryEntities db = new PhotoCalleryEntities();
         int FindRightIndex;
+        int selectedtodelete;
 
 
         public void ComboList()
@@ -37,22 +39,80 @@ namespace PhotoCallery
         {
             var pcb = new PictureBox();
             pcb.Image = Image.FromFile(fullyname);
-            pcb.Top = 5;
+            pcb.Top = top;
             pcb.Width = 160;
             pcb.Height = 160;
             pcb.SizeMode = PictureBoxSizeMode.StretchImage;
             pcb.Left = left;
             left += pcb.Width + 20;
             panel1.Controls.Add(pcb);
+            pcb.Click += ClickinOnPhoto;
 
-            foreach(var x in panel1.Controls)
+
+
+            //for (int y = 0; y < panel1.Controls.Count; y++)
+            //{
+            //    if (panel1.Controls.Count % 4 == 0)
+            //    {
+
+            //        left = 0;
+            //        top += 170;
+
+            //    }
+                
+            //}
+
+
+        }
+
+        private void ClickinOnPhoto(object sender, EventArgs e)
+        {
+            PictureBox px = sender as PictureBox;
+            Button btn = new Button();
+
+            foreach (var x in db.Photos.ToList())
             {
-                if (panel1.Controls.Count % 4 == 0)
-                {
-                    pcb.Top = 170;
-                    pcb.Left = 30;
-                }
+
+                selectedtodelete = x.id;
+
+
             }
+
+            btn.Text = "X";
+            btn.Width = 20;
+            btn.BackColor = Color.Red;
+            btn.ForeColor = Color.White;
+            btn.Click += deletionPhotoo;
+
+            
+
+
+            px.Controls.Add(btn);
+
+            MessageBox.Show("If You want to delete a photo press red Cross");
+
+            ;
+        }
+
+        private void deletionPhotoo(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+
+            var pb = sender as PictureBox;
+
+            foreach (var x in db.Photos.ToList())
+            {
+
+                if (selectedtodelete == x.id)
+                {
+                    MessageBox.Show(selectedtodelete.ToString());
+
+                }
+
+
+            }
+
+
         }
 
         public object AddingPhoto(DateTime daatetime, float FileSize, string FileLocation, string GetExt, int NewNamw)
@@ -79,12 +139,6 @@ namespace PhotoCallery
         {
             InitializeComponent();
             ComboList();
-
-
-
-
-
-
 
 
 
@@ -126,9 +180,9 @@ namespace PhotoCallery
             var FileLocation = $"{NewNamw}{GetExt}";
             var FullnewName = DestinationPath + NewNamw + GetExt;
             File.Copy(RootPath, $"{DestinationPath}{NewNamw}{GetExt}");
-         
 
-            AddingPhoto(daatetime, FileSize, FileLocation, GetExt, NewNamw);           
+
+            AddingPhoto(daatetime, FileSize, FileLocation, GetExt, NewNamw);
             InsertingPhotoToPanel(FullnewName);
 
         }
@@ -180,6 +234,13 @@ namespace PhotoCallery
 
 
             }
+
+
+        }
+
+        public void deletionPhoto()
+        {
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -189,17 +250,19 @@ namespace PhotoCallery
 
         private void categoriesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
 
+
+
+            ToolStripMenuItem MnuStripItem = new ToolStripMenuItem();
+            MnuStripItem.ForeColor = Color.Red;
             foreach (var rw in db.Categories.ToList())
             {
-                
-              ToolStripMenuItem MnuStripItem = new ToolStripMenuItem();
-                MnuStripItem.Text = rw.CategoryName;
+
+                MnuStripItem.ForeColor = Color.Red;
             }
 
         }
 
-        
+
     }
 }
