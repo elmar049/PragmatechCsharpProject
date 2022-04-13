a685bf9055de75af3c54b5551bb3d625f286193e
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-04-13 22:19:14 +0200

    updates

---
+++ Draft/WebApplicationTest/WebApplicationTest/fonts/UserrsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.fonts
+{
+    public class UserrsController : Controller
+    {
+        private TestUserEntities1 db = new TestUserEntities1();
+
+        // GET: Userrs
+        public ActionResult Index()
+        {
+            return View(db.Userr.ToList());
+        }
+
+        // GET: Userrs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Userr userr = db.Userr.Find(id);
+            if (userr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userr);
+        }
+
+        // GET: Userrs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Userrs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,u_name,u_surname,u_phonenumber,u_email,u_date")] Userr userr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Userr.Add(userr);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userr);
+        }
+
+        // GET: Userrs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Userr userr = db.Userr.Find(id);
+            if (userr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userr);
+        }
+
+        // POST: Userrs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,u_name,u_surname,u_phonenumber,u_email,u_date")] Userr userr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userr).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userr);
+        }
+
+        // GET: Userrs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Userr userr = db.Userr.Find(id);
+            if (userr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userr);
+        }
+
+        // POST: Userrs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Userr userr = db.Userr.Find(id);
+            db.Userr.Remove(userr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
