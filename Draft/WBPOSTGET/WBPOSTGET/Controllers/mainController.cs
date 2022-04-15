4fd400279d872dc3730d176f4a85f4a29e1b3a8c
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-04-15 17:19:03 +0200

    updates

---
+++ Draft/WBPOSTGET/WBPOSTGET/Controllers/mainController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WBPOSTGET.Models;
+
+
+namespace WBPOSTGET.Controllers
+{
+    public class mainController : Controller
+    {
+        userlerEntities db=new userlerEntities();
+
+        // GET: main
+
+        public ActionResult Index()
+        {
+            ViewBag.Alluser = db.OneUser.ToList();
+
+           
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(string ad)
+        {
+
+
+
+            //ViewBag.name = Request.Params["adin"];
+            //ViewBag.surname = Request.Params["soyad"];
+            //var singleuser = new OneUser();
+            //singleuser.username = ViewBag.name;
+            //singleuser.usersurname = ViewBag.surname;
+            //ViewBag.Alluser = db.OneUser.ToList();
+            //db.OneUser.Add(singleuser);
+            //db.SaveChanges();
+
+            return View();
+        }
+
+
+
+        public ActionResult Listname()
+        {
+
+            ViewBag.name = Request.Params["adin"];
+            ViewBag.surname = Request.Params["soyad"];
+
+            var singleuser = new OneUser();
+            singleuser.username = ViewBag.name;
+            singleuser.usersurname = ViewBag.surname;
+            ViewBag.Alluser = db.OneUser.ToList();
+            db.OneUser.Add(singleuser);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+       
+       
+
+    }
+}
---
+++ Draft/WebApplication/WebApplication/Controllers/IndexxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class IndexxController : Controller
+    {
+        // GET: Indexx
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
