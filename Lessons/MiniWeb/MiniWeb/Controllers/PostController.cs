ea4c5d98a811804add0e911930298cc55a0faf7a
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-05-10 17:45:31 +0200

    webpage

--- Lessons/MiniWeb/MiniWeb/Controllers/PostController.cs
+++ Lessons/MiniWeb/MiniWeb/Controllers/PostController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MiniWeb.Models;
 
 namespace MiniWeb.Controllers
 {
+    
     public class PostController : Controller
     {
+        BlogEntities db = new BlogEntities();
         // GET: Post
         public ActionResult Index()
         {
+            ViewBag.auth = db.Author.ToList();
+            ViewBag.catg = db.Category.ToList();
             return View();
+        }
+
+        
+        //create () skobkanin icinde, meselcun adi BIND edende, sende modeldeki adi yox, tag Name goturursen formdakini... burda author_id post classna aitdir, ,en ise form-da ona Post Auuthor demiwem)
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "PostAuthor, PostCategory")] Post post, int? PostAuthor, int? PostCategory )
+        {
+            post.Author_Id = PostAuthor;
+            post.Category_Id = PostCategory; ;
+            db.Post.ToList();
+            
+
+
+            db.Post.Add(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");  
         }
     }
 }
