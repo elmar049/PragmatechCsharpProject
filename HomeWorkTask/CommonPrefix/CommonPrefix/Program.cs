3041b3125d47dd7d7493bf46c7e5f62caedeaac5
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-06-18 15:43:58 +0200

    DB MOdel photo

---
+++ HomeWorkTask/CommonPrefix/CommonPrefix/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            List<string> list = new List<string>()
+            {
+                "Elmar", "Elman", "Elxan", "Ekber", "Saleh"
+            };
+
+            List<string> newlist = new List<string>();
+
+            
+
+            for (int y = 0; y < list.Count; y++)
+            {
+                for(int x = 0; x < list[y].Length; x++)
+                {
+                  if(list[y][x] == list[x])
+                    
+                }
+                
+            }
+
+
+
+
+           
+
+           
+
+        }
+    }
+}
