d51e33ee3abc0ea4ec9bfc30bc50b72507a3a3ba
Mammadov Elmar <mamedovelmar93@gmail.com>
2022-06-21 14:54:40 +0200

    DB MOdel photo

--- HomeWorkTask/CommonPrefix/CommonPrefix/Program.cs
+++ HomeWorkTask/CommonPrefix/CommonPrefix/Program.cs
@@ -11,32 +11,43 @@ namespace CommonPrefix
     {
         static void Main(string[] args)
         {
-         
-            List<string> list = new List<string>()
+
+
+            string[] strs = { "Vaaaaaaaaaal", "Vaaal", "Val", "Vaaa" };
+
+            Console.WriteLine(Nese(strs));
+
+
+
+            //sual 1: niye  39cu setride biz Substring eleyirik, ve sonra hemen substringi call edende 
+            //ferqli olur, amma menim examplimimpada eyni olur?
+            string y = "elmar";
+            Console.WriteLine(y);
+            y.Substring(0, 2);
+            Console.WriteLine(y);
+
+            //sual 2:
+            //w ferqi varki men burda return eledim yoxsaki Void ile verdim, eyni sheyi men burda CW verdim amma eror verdi;
+
+
+        }
+        public static string Nese (string [] strs)
+        {
+            for (int i = 0; i < strs[0].Length; i++)
             {
-                "Elmar", "Elman", "Elxan", "Ekber", "Saleh"
-            };
-
-            List<string> newlist = new List<string>();
-
-            
-
-            for (int y = 0; y < list.Count; y++)
-            {
-                for(int x = 0; x < list[y].Length; x++)
+                char tmpChar = strs[0][i];
+                for (int j = 0; j < strs.Length; j++)
                 {
-                  if(list[y][x] == list[x])
+                    if (strs[j].Length == i || strs[j][i] != tmpChar)
+                    {
+                        return strs[0].Substring(0, i);
+
+
+                    }
                     
                 }
-                
             }
-
-
-
-
-           
-
-           
+            return strs[0];
 
         }
     }
